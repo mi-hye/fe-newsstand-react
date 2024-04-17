a7a1ce3095a174ff39510184fb9c107d6640b15b
mi-hye <satge13@gmail.com>
2024-04-17 11:36:32 +0900

    feat: Implement Header

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+
+function App() {
+	return (
+		<div className="flex justify-center items-center w-screen h-screen dark:bg-slate-950">
+			<div className="w-1000 h-600">
+				<Header />
+				<div>묭??</div>
+				<div>늉</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+const initDate = () =>
+	new Intl.DateTimeFormat("ko-KR", {
+		year: "numeric",
+		month: "numeric",
+		day: "numeric",
+		weekday: "long",
+	}).format(new Date());
+
+const toggleDarkMode = () => document.body.classList.toggle("dark");
+
+function Header() {
+	return (
+		<header className="p-7 w-full flex justify-between items-center">
+			<a href="#" className="dark:text-white font-extrabold text-xl">
+				<i className="fa-solid fa-newspaper mr-2 text-news-blue"></i>뉴스스탠드
+			</a>
+			<i
+				onClick={toggleDarkMode}
+				className="fa-solid fa-moon text-2xl cursor-pointer hover:animate-wiggle transition-colors dark:text-yellow-400"
+			></i>
+			<span className="text-sm dark:text-white">{initDate()}</span>
+		</header>
+	);
+}
+
+export default Header;
