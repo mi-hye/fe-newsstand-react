8979ad8432859f035399004a42b8a7bd967a0d76
mi-hye <satge13@gmail.com>
2024-04-17 11:35:48 +0900

    design: Update design in header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	darkMode: "selector",
 	theme: {
-		extend: {},
+		extend: {
+			width: {
+				1000: "1000px",
+			},
+			height: {
+				600: "600px",
+			},
+			colors: {
+				"news-blue": "#4361cf",
+			},
+			animation: {
+				wiggle: "wiggle 1s ease-in-out infinite",
+			},
+			keyframes: {
+				wiggle: {
+					"0%, 100%": { transform: "rotate(-10deg)" },
+					"50%": { transform: "rotate(10deg)" },
+				},
+			},
+		},
 	},
 	plugins: [],
 };
