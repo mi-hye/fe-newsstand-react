dfd4848e8352a9658d8a9d8e1f8505f659e12c48
mi-hye <satge13@gmail.com>
2024-04-19 14:01:39 +0900

    feat: Add grid items

--- src/components/Main/Grid/TotalGrid.tsx
+++ src/components/Main/Grid/TotalGrid.tsx
@@ -1,5 +1,24 @@
-function TotalGrid() {
-	return <div>토탈그리드임</div>;
+interface Props {
+	news: News[];
+}
+
+const GRID_TOTAL_NUM = 96;
+
+const getTotalGridNews = (news: News[]) =>
+	news.slice(0, GRID_TOTAL_NUM).sort(() => Math.random() - 0.5);
+
+function TotalGrid({ news }: Props) {
+	const gridNews = getTotalGridNews(news);
+
+	return (
+		<div className="h-full grid grid-rows-4 grid-cols-6">
+			{Array.from({ length: 24 }).map((_, i) => (
+				<div className="border-b-2 border-r-2 border-customGray flex justify-center items-center hover:bg-news-gray/10 dark:border-white/40">
+					<img className="h-6" src={gridNews[i].logoImageSrc} alt={gridNews[i].id}></img>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default TotalGrid;
