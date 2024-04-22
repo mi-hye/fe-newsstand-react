7d1ff585376aba0077bb1d9884c4050910fee196
mi-hye <satge13@gmail.com>
2024-04-22 22:24:30 +0900

    refactor: Handling exceptions

--- src/components/LatestNews/LatesNews.tsx
+++ src/components/LatestNews/LatesNews.tsx
@@ -4,16 +4,11 @@ import RollingNews from "./RollingNews/RollingNews";
 
 function LatesNews() {
 	const [news] = useContext(NewsContext);
+	if (!news.length) return <div>데이터를 불러오지 못했음</div>;
 	return (
 		<section className="flex">
-			{news.length ? (
-				<>
-					<RollingNews margin="mr-4" news={news.slice(0, 50)} />
-					<RollingNews news={news.slice(0, 50)} />
-				</>
-			) : (
-				<div>데이터를 불러오지 못했음</div>
-			)}
+			<RollingNews margin="mr-4" news={news.slice(0, 50)} />
+			<RollingNews news={news.slice(0, 50)} />
 		</section>
 	);
 }
