2c9257ea5eadee4f91f52fa36615672547e76a17
mi-hye <satge13@gmail.com>
2024-04-23 18:12:57 +0900

    feat: Implement category length

--- src/NewsProvider.tsx
+++ src/NewsProvider.tsx
@@ -4,10 +4,22 @@ interface Props {
 	children: React.ReactNode;
 }
 const SERVER = process.env.REACT_APP_JSON_SERVER;
-const NewsContext = createContext<[News[]]>([[]]);
+const NewsContext = createContext<[News[], {}]>([[], {}]);
+
+const APIs = {
+	CATEGORY_ECONOMY: `${SERVER}/news?category=종합/경제`,
+	CATEGORY_FINANCE: `${SERVER}/news?category=방송/통신`,
+	CATEGORY_IT: `${SERVER}/news?category=IT`,
+	CATEGORY_ENGLISH_NEWS: `${SERVER}/news?category=영자지`,
+	CATEGORY_SPORTS: `${SERVER}/news?category=스포츠/연예`,
+	CATEGORY_MAGAZINE: `${SERVER}/news?category=매거진/전문지`,
+	CATEGORY_REGION: `${SERVER}/news?category=지역`,
+};
 
 function NewsProvider({ children }: Props) {
 	const [news, setNews] = useState<News[]>([]);
+	const [categoryLength, setCategoryLength] = useState<{}>({});
+
 	useEffect(() => {
 		const fetchNews = async () => {
 			try {
@@ -20,7 +32,26 @@ function NewsProvider({ children }: Props) {
 		};
 		fetchNews();
 	}, []);
-	return <NewsContext.Provider value={[news]}>{children}</NewsContext.Provider>;
+
+	useEffect(() => {
+		const fetchNews = async () => {
+			try {
+				const res = Object.values(APIs).map((api) => fetch(api));
+				const promiseRes = await Promise.all(res);
+				for (const promise of promiseRes) {
+					const category = await promise.json();
+					setCategoryLength((prev) =>
+						Object.assign(prev, { [category[0].category]: category.length })
+					);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchNews();
+	}, []);
+
+	return <NewsContext.Provider value={[news, categoryLength]}>{children}</NewsContext.Provider>;
 }
 
 export { NewsContext, NewsProvider };
--- src/components/Main/List/CategoryTab.tsx
+++ src/components/Main/List/CategoryTab.tsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { NewsContext } from "../../../NewsProvider";
 
 interface Props {
 	currCategory: string;
 }
 
+const style = "pr-14 text-white bg-news-blue/40";
+const categories = [
+	"종합/경제",
+	"방송/통신",
+	"IT",
+	"영자지",
+	"스포츠/연예",
+	"매거진/전문지",
+	"지역",
+];
+
 const TotalCategory = ({ currCategory }: Props) => {
-	const categories = [
-		"종합/경제",
-		"방송/통신",
-		"IT",
-		"영자지",
-		"스포츠/연예",
-		"매거진/전문지",
-		"지역",
-	];
-	const style = "pr-14 text-white bg-news-blue/40";
+	const [, categoryLength] = useContext(NewsContext);
+	console.log(categoryLength);
 	return (
 		<>
 			{categories.map((category, i) => (
@@ -32,7 +36,12 @@ const TotalCategory = ({ currCategory }: Props) => {
 						{category}
 					</span>
 					{category === currCategory ? (
-						<span className="absolute top-0 h-full z-0 bg-news-blue w-full animate-fill"></span>
+						<>
+							<div className="absolute top-[10px] right-2 z-10 text-[10px]">
+								<span>2</span> / 81
+							</div>
+							<span className="absolute top-0 h-full z-0 bg-news-blue w-full animate-fill"></span>
+						</>
 					) : (
 						""
 					)}
@@ -52,7 +61,7 @@ function CategoryTab() {
 	return (
 		<ul
 			onClick={onClick}
-			className="bg-customGray flex items-center border-b-2 h-10"
+			className="bg-customGray flex items-center border-b-2 border-customGray dark:border-white/40 h-10"
 			role="tablist"
 		>
 			<TotalCategory currCategory={currCategory} />
