90c8f7cbae3c41db9fd2c224e6078667c9cad022
mi-hye <satge13@gmail.com>
2024-04-23 14:57:32 +0900

    feat: Add compoments in list news

--- src/components/Main/List/SingleNews/SingleNews.tsx
+++ src/components/Main/List/SingleNews/SingleNews.tsx
@@ -3,11 +3,10 @@ interface Props {
 }
 
 function SingleNews({ singleNews }: Props) {
-	const { id, logoImageSrc, headline, editedTime, subscription } = singleNews;
-	console.log(singleNews);
+	const { id, logoImageSrc, headline, editedTime, subscription, pressName, sideNews } = singleNews;
 	return (
 		<>
-			<div className="flex items-center h-[10%] ml-4">
+			<div className="flex items-center h-[10%] mx-10 my-2">
 				<a className="" href={headline.href}>
 					<img className="h-[20px] mr-4" src={logoImageSrc} alt=""></img>
 				</a>
@@ -16,10 +15,28 @@ function SingleNews({ singleNews }: Props) {
 					id={id}
 					className=" flex items-center text-news-gray border-2 text-xs rounded-xl px-2 py-0.5 bg-white dark:bg-customGray dark:text-white/60"
 				>
-					{subscription ? "해지하기" : "+"}
+					{subscription ? "해지하기" : "+ 구독하기"}
 				</button>
 			</div>
-			<div className="bg-sky-300/10 h-[81%]"></div>
+			<div className="h-[77%] flex">
+				<div className="flex flex-col h-full w-[40%] mx-10">
+					<a href={headline.href}>
+						<img className="w-[330px]" src={headline.thumbnailSrc} alt={pressName}></img>
+					</a>
+					<a className="w-[330px] font-bold my-3 text-sm" href={headline.href}>
+						{headline.title}
+					</a>
+				</div>
+				<ul className="h-full w-[60%] mx-4">
+					{sideNews.map((news, i) => (
+						<li key={i} className={`${i > 0 ? "my-4" : ""}`}>
+							<a className="text-gray-700" href={news.href}>
+								{news.title}
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
 		</>
 	);
 }
