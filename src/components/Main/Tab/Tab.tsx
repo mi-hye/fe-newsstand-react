9e1fceff96b28b09e878fe58bd8aecea113f1212
mi-hye <satge13@gmail.com>
2024-04-23 14:28:02 +0900

    refactor: Change action value

--- src/components/Main/Tab/Tab.tsx
+++ src/components/Main/Tab/Tab.tsx
@@ -10,13 +10,13 @@ function Tab() {
 			<div className="flex items-center">
 				<div
 					className={`cursor-pointer ${state.media === "total" ? bold : "text-news-gray"}`}
-					onClick={() => dispatch({ type: "setTotal" })}
+					onClick={() => dispatch({ type: "SET_VIEW_TOTAL" })}
 				>
 					전체 언론사
 				</div>
 				<div
 					className={`ml-4 cursor-pointer ${state.media === "sub" ? bold : "text-news-gray"}`}
-					onClick={() => dispatch({ type: "setSub" })}
+					onClick={() => dispatch({ type: "SET_VIEW_SUB" })}
 				>
 					내가 구독한 언론사
 				</div>
@@ -26,13 +26,13 @@ function Tab() {
 					className={`fas fa-th-large cursor-pointer text-xl ${
 						state.display === "grid" ? "text-news-blue " : "text-news-gray"
 					}`}
-					onClick={() => dispatch({ type: "setGrid" })}
+					onClick={() => dispatch({ type: "SET_VIEW_GRID" })}
 				></i>
 				<i
 					className={`fa-solid fa-list cursor-pointer text-xl ml-4 ${
 						state.display === "list" ? "text-news-blue" : "text-news-gray "
 					}`}
-					onClick={() => dispatch({ type: "setList" })}
+					onClick={() => dispatch({ type: "SET_VIEW_LIST" })}
 				></i>
 			</div>
 		</nav>
--- src/components/Main/ViewProvider.tsx
+++ src/components/Main/ViewProvider.tsx
@@ -9,19 +9,23 @@ interface ViewState {
 }
 
 //action은 행동이다 "동사+명사로 지어야 명시적임"
-type Action = { type: "setTotal" } | { type: "setSub" } | { type: "setGrid" } | { type: "setList" };
+type Action =
+	| { type: "SET_VIEW_TOTAL" }
+	| { type: "SET_VIEW_SUB" }
+	| { type: "SET_VIEW_GRID" }
+	| { type: "SET_VIEW_LIST" };
 
 const initialState = { media: "total", display: "grid" };
 
 function reducer(state: ViewState, action: Action): ViewState {
 	switch (action.type) {
-		case "setTotal":
+		case "SET_VIEW_TOTAL":
 			return { media: "total", display: "grid" };
-		case "setSub":
+		case "SET_VIEW_SUB":
 			return { media: "sub", display: "list" };
-		case "setGrid":
+		case "SET_VIEW_GRID":
 			return { media: state.media, display: "grid" };
-		case "setList":
+		case "SET_VIEW_LIST":
 			return { media: state.media, display: "list" };
 		default:
 			throw new Error();
