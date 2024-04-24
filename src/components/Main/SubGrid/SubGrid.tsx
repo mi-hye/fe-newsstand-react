441c097274a11d866ae706bae532f549617cfb23
mi-hye <satge13@gmail.com>
2024-04-24 13:47:56 +0900

    feat: Add Swiper

--- src/components/Main/SubGrid/SubGrid.tsx
+++ src/components/Main/SubGrid/SubGrid.tsx
@@ -1,8 +1,10 @@
-import { SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import handleSubscription from "../../../utility/subscription";
+import Swiper from "../Swiper/Swiper";
 
 const SERVER = process.env.REACT_APP_JSON_SERVER;
 const CELL_COUNT = 24;
+const ZERO = 0;
 
 const fetchSubNews = async () => {
 	try {
@@ -16,6 +18,9 @@ const fetchSubNews = async () => {
 
 function SubGrid() {
 	const [subNews, setSubNews] = useState<News[]>([]);
+	const [currentPage, setCurrentPage] = useState<number>(ZERO);
+	const startIdx = currentPage * CELL_COUNT;
+	const lastIdx = subNews.length - 1;
 
 	const getSubNews = async () => {
 		const subNews = await fetchSubNews();
@@ -24,7 +29,7 @@ function SubGrid() {
 
 	useEffect(() => {
 		getSubNews();
-	}, [subNews]);
+	}, []);
 
 	return (
 		<>
@@ -32,20 +37,21 @@ function SubGrid() {
 				onClick={handleSubscription}
 				className=" border-t-2 border-l-2 border-customGray dark:border-white/40 h-full grid grid-rows-4 grid-cols-6"
 			>
-				{subNews.length ? (
-					subNews.map((news) => {
+				{Array.from({ length: CELL_COUNT }).map((_, i) => {
+					if (startIdx + i <= lastIdx) {
+						const currNews = subNews[startIdx + i];
 						return (
 							<div
-								key={news.id}
+								key={currNews.id}
 								className="group border-b-2 border-r-2 border-customGray flex justify-center items-center hover:bg-news-gray/10 dark:border-white/40 dark:hover:bg-news-gray/30"
 							>
 								<img
 									className="h-6 relative group-hover:opacity-0"
-									src={news.logoImageSrc}
-									alt={news.pressName}
+									src={currNews.logoImageSrc}
+									alt={currNews.pressName}
 								></img>
 								<button
-									id={news.id}
+									id={currNews.id}
 									onClick={getSubNews}
 									className="absolute opacity-0 text-gray-400 group-hover:opacity-100 border-2 text-xs rounded-xl px-2 py-0.5 bg-white dark:bg-white/100"
 								>
@@ -53,17 +59,21 @@ function SubGrid() {
 								</button>
 							</div>
 						);
-					})
-				) : (
-					<></>
-				)}
-				{Array.from({ length: CELL_COUNT - subNews.length }).map((_, i) => (
-					<div
-						key={i}
-						className="group border-b-2 border-r-2 border-customGray flex justify-center items-center hover:bg-news-gray/10 dark:border-white/40 dark:hover:bg-news-gray/30"
-					></div>
-				))}
+					}
+					return (
+						<div
+							key={i}
+							className="group border-b-2 border-r-2 border-customGray flex justify-center items-center hover:bg-news-gray/10 dark:border-white/40 dark:hover:bg-news-gray/30"
+						></div>
+					);
+				})}
 			</div>
+			<Swiper
+				lastPage={~~(subNews.length / 24)}
+				currentPage={currentPage}
+				setCurrentPage={setCurrentPage}
+				isGrid={true}
+			/>
 		</>
 	);
 }
