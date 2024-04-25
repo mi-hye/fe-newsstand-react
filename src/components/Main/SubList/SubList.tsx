a4177d86d07dbeb65b8dcce4800e48dcd0721c52
mi-hye <satge13@gmail.com>
2024-04-25 15:38:16 +0900

    feat: Add Swiper

--- src/components/Main/SubList/SubList.tsx
+++ src/components/Main/SubList/SubList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import CategoryTab from "./CategoryTab/CategoryTab";
 import SingleNews from "../shared-components/SingleNews";
+import Swiper from "../shared-components/Swiper";
 
 const SERVER = process.env.REACT_APP_JSON_SERVER;
 const ZERO = 0;
@@ -32,9 +33,19 @@ function SubList() {
 	return (
 		<>
 			<div className="border-2 border-customGray dark:border-white/40 h-full">
-				<CategoryTab subNews={subNews} currCategoryIdx={currCategoryIdx} setcurrCategoryIdx={setcurrCategoryIdx}></CategoryTab>
+				<CategoryTab
+					subNews={subNews}
+					currCategoryIdx={currCategoryIdx}
+					setcurrCategoryIdx={setcurrCategoryIdx}
+				></CategoryTab>
 				<SingleNews singleNews={subNews[currCategoryIdx]}></SingleNews>
 			</div>
+			<Swiper
+				lastPage={subNews.length - 1}
+				currentPage={currCategoryIdx}
+				setCurrentPage={setcurrCategoryIdx}
+				isGrid={false}
+			></Swiper>
 		</>
 	);
 }
