640265378ef7a8057bfce42706c6544c70797efa
mi-hye <satge13@gmail.com>
2024-04-25 14:38:10 +0900

    design: Modify design & Add img scale

--- src/components/Main/shared-components/SingleNews.tsx
+++ src/components/Main/shared-components/SingleNews.tsx
@@ -8,9 +8,9 @@ function SingleNews({ singleNews }: Props) {
 		<>
 			<div className="flex items-center h-[10%] ml-10 my-2 w-[36%] justify-between">
 				<a className="" href={headline.href}>
-					<img className="h-[20px] mr-4" src={logoImageSrc} alt=""></img>
+					<img className="h-[20px] mr-3" src={logoImageSrc} alt=""></img>
 				</a>
-				<span className="mr-4 text-sm dark:text-white/60">{editedTime}</span>
+				<span className="mb-1 mr-3 text-sm dark:text-white/60">{editedTime}</span>
 				<button
 					id={id}
 					className=" flex items-center text-news-gray border-2 text-xs rounded-xl px-2 py-0.5 bg-white dark:bg-customGray dark:text-white/60"
@@ -20,21 +20,27 @@ function SingleNews({ singleNews }: Props) {
 			</div>
 			<div className="h-[77%] flex">
 				<div className="flex flex-col h-full w-[40%] mx-10">
-					<a href={headline.href}>
+					<a className="overflow-hidden max-w-[340px]" href={headline.href}>
 						<img
-							className="w-[330px] hover:scale-110"
+							className="w-[340px] transition-transform	duration-300 hover:scale-110"
 							src={headline.thumbnailSrc}
 							alt={pressName}
 						></img>
 					</a>
-					<a className="w-[330px] font-bold my-3 text-sm dark:text-white" href={headline.href}>
+					<a
+						className="w-[340px] font-bold my-3 text-sm dark:text-white hover:underline"
+						href={headline.href}
+					>
 						{headline.title}
 					</a>
 				</div>
 				<ul className="h-full w-[60%] mx-4">
 					{sideNews.map((news, i) => (
 						<li key={i} className={`${i > 0 ? "my-4" : ""}`}>
-							<a className="text-gray-700 dark:text-white/70 text-sm" href={news.href}>
+							<a
+								className="text-gray-700 dark:text-white/70 text-sm hover:underline"
+								href={news.href}
+							>
 								{news.title}
 							</a>
 						</li>
