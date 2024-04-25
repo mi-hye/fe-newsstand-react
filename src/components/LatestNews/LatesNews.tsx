561b51e7fd36abde5dc174dd6c1170f0e17bb6ad
mi-hye <satge13@gmail.com>
2024-04-25 20:58:14 +0900

    refactor: Add fetch News

--- src/components/LatestNews/LatesNews.tsx
+++ src/components/LatestNews/LatesNews.tsx
@@ -1,14 +1,19 @@
-// import { useContext } from "react";
-// import { NewsContext } from "../../NewsProvider";
+import { useEffect, useState } from "react";
+import { getTotalNews } from "../../utility/newsSetting";
 import RollingNews from "./RollingNews/RollingNews";
 
 function LatesNews() {
-	// const [news] = useContext(NewsContext);
-	// if (!news.length) return <div>데이터를 불러오지 못했음</div>;
+	const [news, setNews] = useState<News[]>([]);
+
+	useEffect(() => {
+		getTotalNews(setNews);
+	}, []);
+
+	if (!news.length) return <div className="my-5 text-xl text-center font-bold">|ω・`)ﾉ Hi..</div>;
 	return (
 		<section className="flex">
-			{/* <RollingNews margin="mr-4" news={news.slice(0, 50)} />
-			<RollingNews news={news.slice(0, 50)} /> */}
+			<RollingNews margin="mr-4" news={news.slice(0, 50)} />
+			<RollingNews news={news.slice(0, 50)} />
 		</section>
 	);
 }
