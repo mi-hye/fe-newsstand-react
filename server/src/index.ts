35e83d983e8095edcc04db80cfddbb972d8cc646
mi-hye <satge13@gmail.com>
2024-09-04 18:33:27 +0900

    chore: Add comment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import APIRouter from "./routes/router";
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(express.json()); //요청 본문 파싱을 위한 미들웨어 설정
 app.use("/", APIRouter);
 
-const PORT = process.env.PORT; //DELETE
-app.listen(PORT, () => console.log("서버응답", PORT));
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => console.log("서버응답", PORT));
 module.exports = app; // Vercel에서 서버리스 함수로 인식
