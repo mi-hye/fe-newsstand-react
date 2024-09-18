b4fb77213b4a076c1fa76fe18322f7707b1072e9
mi-hye <satge13@gmail.com>
2024-09-18 18:25:47 +0900

    Setting: Install react query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,25 @@ import Header from "./components/Header/Header";
 import LatesNews from "./components/LatestNews/LatesNews";
 import Main from "./components/Main/Main";
 import { NewsProvider } from "./NewsProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<div className="flex justify-center items-center w-screen h-screen dark:bg-slate-950">
-			<div className="w-1000 h-600">
-				<Header />
-				<NewsProvider>
-					<LatesNews />
-					<Main></Main>
-				</NewsProvider>
+		<QueryClientProvider client={queryClient}>
+			<div className="flex justify-center items-center w-screen h-screen dark:bg-slate-950">
+				<div className="w-1000 h-600">
+					<Header />
+					<NewsProvider>
+						<LatesNews />
+						<Main></Main>
+					</NewsProvider>
+				</div>
 			</div>
-		</div>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
 
